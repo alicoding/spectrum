fe01f2498c1ae218fd9acdfa626bede5f84db9e8
Ali Al Dallal <ali@alicoding.com>
2013-07-02 23:07:36 -0400

    Added new setup for ES

--- config/ESconfig.js
+++ config/ESconfig.js
@@ -6,92 +6,11 @@ var
   },
   es = elasticsearch(config);
 
-
-var options = { _index : "spectrum", _type: "post" },
-mapping = {
-
-
-"spectrum" : {
-    "post" : {
-      "properties" : {
-        "author" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "contentType" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "createdAt" : {
-          "type" : "long"
-        },
-        "deletedAt" : {
-          "type" : "long"
-        },
-        "description" : {
-          "type" : "string"
-        },
-        "email" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "locale" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "published" : {
-          "type" : "object"
-        },
-        "remixedFrom" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "tags" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "thumbnail" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        },
-        "title" : {
-          "type" : "string"
-        },
-        "updatedAt" : {
-          "type" : "long"
-        },
-        "url" : {
-          "type" : "string",
-          "index" : "not_analyzed",
-          "omit_norms" : true,
-          "index_options" : "docs"
-        }
-      }
-    }
+es.indices.exists(config, function (err, data) {
+  if (err) {
+    es.indices.createIndex(function (err, data) {
+    });
   }
-
-};
-
-es.indices.putMapping(options, mapping, function ( err, result) {
-	if(err) {
-		return console.log("error" + err.message);
-	}
-	console.log("succeed");
-})
-
+});
 
 module.exports = es;
